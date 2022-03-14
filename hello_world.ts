e9fd5cd599b2f47b4d60f9057f784a0984d80bde
gelareh1996 <79374175+gelareh1996@users.noreply.github.com>
2022-03-13 21:54:54 -0400

    Add files via upload

---
+++ hello_world.ts
@@ -0,0 +1,4 @@
+var greeter =function(name){
+    console.log('hello' +name)
+}
+greeter("gelare")
---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var customer_1 = require("./customer");
+var customer = new customer_1.Customer("gelareh", "farhoudi", 26);
+customer.greeter();
+customer.getAge();
